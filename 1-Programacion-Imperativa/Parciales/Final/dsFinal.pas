f073c8bda1043e6d27c8440d4238e1ccceeb8952
Nahuel Arancibia <100500003+NahuelArn@users.noreply.github.com>
2023-09-02 13:12:36 -0300

    CompiladoYprobado.pas

--- 1-Programacion-Imperativa/Parciales/Final/dsFinal.pas
+++ 1-Programacion-Imperativa/Parciales/Final/dsFinal.pas
@@ -257,12 +257,8 @@ end.
 
 
 
-
-
-
 {VARIACION AYRTON}
 
-program p2;
 {2.- Una agencia dedicada a la venta de autos ha organizado su stock y, dispone en papel de la información de los autos en venta.
 Implementar un programa que:
 a) Genere un árbol binario de búsqueda ordenado por patente identificatoria del auto en venta. Cada nodo del árbol debe contener patente,
@@ -270,6 +266,8 @@ a) Genere un árbol binario de búsqueda ordenado por patente identificatoria de
 b) Invoque a un módulo que reciba el árbol generado en a) y una marca y retorne la cantidad de autos de dicha marca que posee la agencia. Mostrar el resultado. 
 c) Invoque a un módulo que reciba el árbol generado en a) y retorne una estructura con la información de los autos agrupados por año de fabricación.
 d) Contenga un módulo que reciba el árbol generado en a) y una patente y devuelva el año de fabricación del auto con dicha patente. Mostrar el resultado. }
+{
+program p2;
 Type
 	auto = record
 		patente:string;
@@ -443,3 +441,4 @@ BEGIN
 	//organizarPorAnio(A, autos);
 	buscarAuto(A);
 END.
+}
